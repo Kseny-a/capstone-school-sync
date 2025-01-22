608875642aab18f7016f53fc0eaa31dbde35ec36
Aksana <adziamidava@gmail.com>
2025-01-22 12:52:18 -0800

    implemented app listening to FB data

--- src/app/components/events/dashboard/EventDashboard.tsx
+++ src/app/components/events/dashboard/EventDashboard.tsx
@@ -1,9 +1,11 @@
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
 import EventForm from "../form/EventForm";
-import { sampleData } from "../../../api/sampleData";
+// import { sampleData } from "../../../api/sampleData";
 import { useEffect, useState } from 'react'
 import { AppEvent } from "../../../types/event"
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import { db } from '../../../api/config/firebase'
 
 type Props = {
   showForm: boolean
@@ -14,9 +16,21 @@ function EventDashboard({ showForm, setShowForm }: Props) {
   
   const [eventData, setEventData] = useState<AppEvent[]>([])
 
-  useEffect(() => {
-    setEventData(sampleData)
-  }, [])
+  useEffect(()=> {
+    const q = query(collection(db, 'events'));
+    const unsubscribe = onSnapshot(q, {
+      next: querySnapshot => {
+        const events: AppEvent[] = [];
+        querySnapshot.forEach(doc => {
+          events.push({id: doc.id, ...doc.data()} as AppEvent)
+        })
+        setEventData(events);
+      },
+      error: error => console.log(error),
+      complete: () => console.log('done') 
+  });
+  return () => unsubscribe()
+  }, []);
 
   // add event to Evenlist
   const addEvent = (event: AppEvent) => {
