fb82cf73a3ba01efff2bee05f9c3f726edcae9a2
Brianna Root <brianna.k.root@gmail.com>
2025-01-27 12:37:00 +1300

    added more event details and props to events detail page.

--- src/app/features/events/page/EventDetailedHeader.tsx
+++ src/app/features/events/page/EventDetailedHeader.tsx
@@ -1,36 +1,73 @@
-import { Segment, Item, Header, Button } from "semantic-ui-react"
+import { Link } from "react-router-dom"
+import { Segment, Item, Header, Button, Grid, Icon } from "semantic-ui-react"
+import { AppEvent } from "../../../types/event"
 
-export default function EventDetailedHeader() {
+
+type Props={
+  event: AppEvent
+}
+export default function EventDetailedHeader({event}: Props) {
   return (
     <Segment.Group>
-    <Segment basic attached="top" style={{padding: '0'}}>
+      <Segment basic attached="top" style={{ padding: '0' }}>
         <Segment basic>
-            <Item.Group>
-                <Item>
-                    <Item.Content>
-                        <Header
-                            size="huge"
-                            content='Event Title'
-                            style={{color: 'teal'}}
-                        />
-                        <p>Event Date</p>
-                        <p>
-                            Hosted by <strong>Bob</strong>
-                        </p>
-                    </Item.Content>
-                </Item>
-            </Item.Group>
+          <Item.Group>
+            <Item>
+              <Item.Content>
+                <Header
+                  size="huge"
+                  content={event.title}
+                  style={{ color: 'teal' }}
+                />
+                <p>Hosted by {event.hostedBy}</p>
+              </Item.Content>
+            </Item>
+          </Item.Group>
         </Segment>
-    </Segment>
+      </Segment>
 
-    <Segment attached="bottom">
+      <Segment attached>
         <Button>Unattend</Button>
         <Button>Attend</Button>
 
-        <Button floated="right">
-            Manage Event
+        <Button as={Link} to={`/manage/${event}`}floated="right">
+          Manage Event
         </Button>
-    </Segment>
-  </Segment.Group>
+      </Segment>
+
+      <Segment attached>
+        <Grid>
+          <Grid.Column width={15}>
+            <Icon color="teal" name="info" />
+            <span>Event Details</span>
+          </Grid.Column>
+            <span>{event.description}</span>
+          <Grid.Column width={15}>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+      <Segment attached>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={1}>
+            <Icon name="calendar" color="teal" />
+          </Grid.Column>
+          <Grid.Column width={11}>
+            <span>{event.date.toDate().toLocaleString()}</span>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+      <Segment attached>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={1}>
+            <Icon name="marker" color="teal" />
+          </Grid.Column>
+          <Grid.Column width={11}>
+            <span>{event.venue}</span>
+            <p>{event.address}</p>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    </Segment.Group>
+
   )
 }
